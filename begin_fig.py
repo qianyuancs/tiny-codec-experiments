47413be4ad49136ca8a055f7447eaa97b4963870
Qian Yuan <202420119011@bfsu.edu.cn>
2025-09-02 21:47:59 +0800

    Update begin_fig.py

--- begin_fig.py
+++ begin_fig.py
@@ -1,7 +1,7 @@
 import librosa
 import matplotlib.pyplot as plt
 
-y, sr = librosa.load(r"C:\Users\21978\Desktop\code\warm_up\data\LibriSpeech\samples\sample_0.wav", sr=16000)
+y, sr = librosa.load(r"data\LibriSpeech\samples\sample_0.wav", sr=16000)
 plt.figure(figsize=(10, 3))
 plt.plot(y)
 plt.title("Waveform Example (16kHz)")
